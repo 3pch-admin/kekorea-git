dca3c272ec20c31053895942fbdaa4452613280e
3p_admin <3p_admin@3pchain.co.kr>
2024-05-20 10:25:06 +0900

    1. 국체 이클립스 초반 세팅 데이터

---
+++ kekorea/codebase/extcore/component/AUIGrid/AUIGridLicense_dev.js
@@ -0,0 +1,10 @@
+/*!
+ * AUIGrid 3.0.14 License
+ * License Type : Evaluation(Trial) Version
+ * Authorized Domain(or IP) : localhost,127.0.0.1,plmdev.kekorea.co.kr
+ * Expiration Date : 2024-05-03
+ * www.auisoft.net
+ */
+ /* eslint-disable */
+var AUIGridLicense = "eyJjdCI6IlVMRDFWZ3h1MzlnY3NxREh3RzBZeEdYc2d4QkNZUFVHblJQRmo1N0pGNVlmUW9tb1d5UHNSemVhNHhjc1FtWEk0WXJKdXd6MkNEaERZRFR0N1VWUVRwUU0zZG1yRWpoZlRkb0h2bzZ6TzU4ZWRPMVFuUWVSVVBjY2hJRnVPVGRPIiwiaXYiOiJiNzBmODJhYmQ4NzQzOWU5NTAzMDljMDlhODVjM2QwOSIsInMiOiIwOTgyM2I3YzNmM2FlN2JhIn0=";
+if (typeof window !== "undefined") window.AUIGridLicense = AUIGridLicense;
